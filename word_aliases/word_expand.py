8a0c7f960dc94ff6b91ae0ed03dcca151af7a81e
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2021-03-14 08:47:56 +0700

    Modify word_expand.py - detect words in output that does not appear in input

--- word_aliases/word_expand.py
+++ word_aliases/word_expand.py
@@ -1,31 +1,43 @@
 import csv
+import pprint
 from collections import defaultdict
 
 with open('scl-2021-ds/train.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     aliases = {}
-    for row in csv_reader:
-        if line_count != 0:
-            raw_address = row[1]
-            raw_address = raw_address.replace(',', '')
-            raw_words = raw_address.split()
+    for line_count, row in enumerate(csv_reader):
+        if line_count == 0:
+            continue
+        raw_address = row[1]
+        raw_address = raw_address.replace(',', '')
+        raw_words = raw_address.split()
 
-            actual_address = row[2].split("/")[1]
-            actual_address = actual_address.replace(',', '')
-            actual_address = actual_address.split()
+        actual_address = row[2].replace('/', ' ')
+        actual_address = actual_address.replace(',', ' ')
+        actual_address = actual_address.split()
 
-            for word in actual_address:
+        for word in actual_address:
+            if word not in raw_words:
                 for abbrev in raw_words:
                     if (abbrev in word) and (abbrev != word) and (len(abbrev) >
-                                                                  1):
+                                                                1):
                         if word not in aliases:
                             aliases[word] = {}
                         if abbrev not in aliases[word]:
                             aliases[word][abbrev] = 1
                         else:
                             aliases[word][abbrev] += 1
-        line_count += 1
+                if word in aliases:
+                    if '_count' not in aliases[word]:
+                        aliases[word]['_count'] = 1
+                    else:
+                        aliases[word]['_count'] += 1
 
-    print(aliases)
+pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(aliases)
 
+aliases_list = list(aliases.items())
+print(aliases_list[:10])
+aliases_list.sort(key = lambda tup: tup[1]['_count'], reverse=True)
+pp.pprint(aliases_list)
